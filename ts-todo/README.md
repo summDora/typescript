509c4e547460a16fe87894255378052750df3b02
summerDora <15988484252@163.com>
2020-11-16 14:56:52 +0800

    vscode prettier 缩进 单引号 格式化问题

---
+++ ts-todo/README.md
@@ -0,0 +1,28 @@
+# ts-todo
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### vscode prettier
+```
+在prettier issue中发现了原因所在，prettier3.7以上会优先读取项目根目录下的editorconfig or prettier config，如果有，就不会使用vscode setting中的设置，所以最好每个项目都配置一个单独的 prettier 配置 文件
---
+++ ts-todo/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Home',
+  components: {}
+})
+</script>
